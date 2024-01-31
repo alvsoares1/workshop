b7093259917bd5a79f2d2afd0a234ab43173f506
alvsoares1 <alvaroass2110@gmail.com>
2024-01-31 10:07:48 -0300

    conectando database MongoDB, criação de camadas do backend

---
+++ src/main/java/com/example/workshop/services/UserService.java
@@ -0,0 +1,18 @@
+package com.example.workshop.services;
+
+import com.example.workshop.domain.User;
+import com.example.workshop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+        return repo.findAll();
+    }
+}
