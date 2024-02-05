de73944720cdf5340a429e075d8311f26d816008
alvsoares1 <alvaroass2110@gmail.com>
2024-02-05 09:38:00 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/example/workshop/domain/User.java
+++ src/main/java/com/example/workshop/domain/User.java
@@ -3,10 +3,12 @@ package com.example.workshop.domain;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 @Document(collection = "user")
 public class User implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     @Id
     private String id;
--- src/main/java/com/example/workshop/resources/UserResource.java
+++ src/main/java/com/example/workshop/resources/UserResource.java
@@ -3,15 +3,13 @@ package com.example.workshop.resources;
 import com.example.workshop.domain.User;
 import com.example.workshop.dto.UserDTO;
 import com.example.workshop.services.UserService;
-import org.apache.coyote.Response;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
